3da1f837670b7446cbd5d92c6aae4e6190299be1
suminda123 <suminda123@gmail.com>
2016-03-15 00:47:37 +1300

    gallery

--- public/js/init.js
+++ public/js/init.js
@@ -2,6 +2,11 @@
 	$(function(){
 
 		$('.button-collapse').sideNav();
-
+		
+		if($(".fancybox").length>0)
+			$(".fancybox").fancybox({
+					prevEffect		: 'elastic',
+					nextEffect		: 'elastic'});
+		
 	}); // end of document ready
 })(jQuery); // end of jQuery name space
