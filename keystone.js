8ed3533c173e6fdc5416edb1a8f04c0bbfc939c3
suminda123 <suminda123@gmail.com>
2016-09-24 16:40:59 +1200

    update to latest

--- keystone.js
+++ keystone.js
@@ -52,39 +52,6 @@ keystone.set('routes', require('./routes'));
 // Setup common locals for your emails. The following are required by Keystone's
 // default email templates, you may remove them if you're using your own.
 
-keystone.set('email locals', {
-	logo_src: '/images/logo-email.gif',
-	logo_width: 194,
-	logo_height: 76,
-	theme: {
-		email_bg: '#f9f9f9',
-		link_color: '#2697de',
-		buttons: {
-			color: '#fff',
-			background_color: '#2697de',
-			border_color: '#1a7cb7'
-		}
-	}
-});
-
-// Setup replacement rules for emails, to automate the handling of differences
-// between development a production.
-
-// Be sure to update this rule to include your site's actual domain, and add
-// other rules your email templates require.
-
-keystone.set('email rules', [{
-	find: '/images/',
-	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/images/' : 'http://localhost:3001/images/'
-}, {
-	find: '/keystone/',
-	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/keystone/' : 'http://localhost:3001/keystone/'
-}]);
-
-// Load your project's email test routes
-
-keystone.set('email tests', require('./routes/emails'));
-
 // Configure the navigation bar in Keystone's Admin UI
 
 keystone.set('nav', {
